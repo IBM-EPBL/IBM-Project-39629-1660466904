d8f2cf7e936001bbb948b7222338f87d9f65d742
kalai51 <64304448+kalai51@users.noreply.github.com>
2022-11-19 11:16:55 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 4/README.md
@@ -0,0 +1,12 @@
+### Sprint 4 - Adding NewsApi and IBM Watson-Chatbot
+
+- Used NewsApi (Free version) - https://newsapi.org/ , documentation - https://newsapi.org/docs
+- Separate module from above 3 Sprint .
+- Home News Feed - with general daily news.
+- Feeds available for general Category wise - (Bussiness, Technology, Entertainment, Science, Sports & Health)
+- Customized feed for the User as their Preferences from the DB, is Showed under MY FEED - button.
+- IBM Watson Chatbot - helps to get short news (in 30 words).
+- CODE DESCRIPTION
+- views.py has all path and redirectories.
+- request.py has various NewsApi requests.
+- templates\base.html is base HTML for every News feed and has IBM Watson.
---
+++ Project Development Phase/Sprint 4/views.py
@@ -0,0 +1,122 @@
+from app import app
+from flask import render_template
+from .request import businessArticles, entArticles, get_news_source, healthArticles, publishedArticles, randomArticles, scienceArticles, sportArticles, techArticles, topHeadlines
+from flask import Flask, render_template, request, redirect, session,flash
+import sqlite3 as sql
+
+# app = Flask(__name__)
+app.secret_key = 'fasdgfdgdfg'
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+   return render_template('Signup.html')
+
+@app.route('/login')
+def login():
+   return render_template('Login.html')
+
+@app.route('/signupfn',methods = ['POST', 'GET'])
+def signupfn():
+   if request.method == 'POST':
+      try:
+         emailid = request.form['emailid']
+         passwrd = request.form['password']
+         usrname = request.form['username']
+         
+         with sql.connect("ntapp.db") as con:
+         #    cur1 = con.cursor()
+         #    cur1.execute("select * from user_login where email=?",(emailid))
+         #    check=cur1.rowcount
+         # if(check!=0):
+         #    error1="User with this Email ID already Exists !!"
+         # else:
+            cur = con.cursor()
+            cur.execute("INSERT INTO user_login (email,password,name) VALUES (?,?,?)",(emailid,passwrd,usrname) )
+            con.commit()
+            error1="User Sign Up Successfull ! Proceed Login"
+            flash("Record successfully added!")
+      except:
+         con.rollback()
+      
+      finally:
+         return render_template("Signup.html",error="     User Sign Up Successfull ! Proceed Login")
+         con.close()
+
+@app.route('/loginfn',methods = ['POST', 'GET'])
+def loginfn():  
+    id = request.form["emailid"]  
+    with sql.connect("ntapp.db") as con:  
+        try:  
+            cur = con.cursor()  
+            cur.execute("select email from user_login")  
+            records=cur.fetchall
+            msg = "record successfully deleted"  
+        except:  
+            msg = "can't be deleted"  
+        finally:  
+            return render_template("category.html") 
+ 
+@app.route('/newsfeed')
+def newsfeed():
+    articles = publishedArticles()
+
+    return  render_template('newsfeed.html', articles = articles)
+
+
+@app.route('/headlines')
+def headlines():
+    headlines = topHeadlines()
+
+    return  render_template('headlines.html', headlines = headlines)
+
+@app.route('/articles')
+def articles():
+    random = randomArticles()
+
+    return  render_template('articles.html', random = random)
+
+@app.route('/sources')
+def sources():
+    newsSource = get_news_source()
+
+    return  render_template('sources.html', newsSource = newsSource)
+
+@app.route('/category/business')
+def business():
+    sources = businessArticles()
+
+    return  render_template('business.html', sources = sources)
+
+@app.route('/category/tech')
+def tech():
+    sources = techArticles()
+
+    return  render_template('tech.html', sources = sources)
+
+@app.route('/category/entertainment')
+def entertainment():
+    sources = entArticles()
+
+    return  render_template('entertainment.html', sources = sources)
+
+@app.route('/category/science')
+def science():
+    sources = scienceArticles()
+
+    return  render_template('science.html', sources = sources)
+
+@app.route('/category/sports')
+def sports():
+    sources = sportArticles()
+
+    return  render_template('sport.html', sources = sources)
+
+@app.route('/category/health')
+def health():
+    sources = healthArticles()
+
+    return  render_template('health.html', sources = sources)
